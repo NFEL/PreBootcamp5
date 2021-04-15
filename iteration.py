8af006b5f3f9e99e470a4daa53fffebe933ba1da
navid <nfilsaraee@gmail.com>
2021-04-15 17:10:35 +0430

    first session

---
+++ iteration.py
@@ -0,0 +1,251 @@
+# my_wallet = 2500
+
+# ali_wallet = 3500
+
+# sahar_wallet = 5000
+
+
+# percentage_ali =  ali_wallet / (my_wallet + ali_wallet + sahar_wallet) * 100
+
+
+# #  int /   int => float
+# #  int //  int => int 
+
+# # 5 / 2 => 2.5
+# # 5 // 2 => 2
+
+
+# # print(percentage_ali)
+
+# # # ()
+# # # **
+# # #  *  / 
+# # #   + -
+
+# # print(type(percentage_ali))
+# # int_shode =  int(percentage_ali)
+# # print(type(int_shode))
+
+# # Type casting
+
+# # vordi1 = "2500"
+# # vordi2 = "5500"
+
+# # print(int(vordi1) + int(vordi2))
+
+# # print(type(float(vordi1)))
+
+# # int => decimal
+# # binary 
+# vordi = 100.0
+
+
+# # print( int(vordi , 5) )
+
+# print( str(vordi) )
+
+
+
+
+# if vordi > 25 :
+#     print("add bozorg")
+
+# # if vordi > "25" :
+# #     print("ajibe")
+    
+
+# #  a - z
+# #  A - Z
+
+
+# if "navid" > "Ahmad" :
+#     print("nice")
+    
+# if "navid" > "ahmad" :
+#     print("nice")
+
+
+# # if condiction
+# # >    >=
+# # <    <=
+# # ==   !=  
+
+# num = 0
+
+# if num < -2: 
+#     print("shart false")
+    
+# elif num < -100:
+#     print("eshteb kasD")
+    
+# elif num <= -200:
+#     print("adde kochek tar az 200")
+
+# # elif not num == 1500 :
+# #     print("avre 1500 nist")
+
+# # elif num != 0:
+# #     print("added ghier sefr")
+
+# elif not num :
+#     print("added sefr")
+
+# else:
+#     print("in shart dar nahayat ejra mishavad")
+
+
+# # Logical opretaions
+# #   and => و
+# #   or => یا
+# #   not => نه این
+    
+# #   -100
+# #   +100
+
+# if not ( -100 < num and num < 100 ):
+#     print("able")
+# # -100 > num or num > 100
+
+        
+# if -100 < num and num < 100:
+#     print("Khob addet beyne -100 ta 100 hast")
+
+
+# # >>> num = 3500
+# # >>> print(not ( -100 < num and num < 100 ))
+# # True
+# # >>> shart = not ( -100 < num and num < 100 )
+# # >>> shart 
+# # True
+# # >>> num = 50
+# # >>> shart 
+# # True
+# # >>> shart = not ( -100 < num and num < 100 )
+# # >>> shart 
+# # False
+# # >>> 
+
+
+
+
+
+# Variables  
+# Types
+# If statments
+# For 
+# Iteration 
+# Loop
+
+# Data Structure
+
+# 1 , 2 , 3 , 4 , 5 
+
+# for i in range(6):
+#     print(i,end=" - ") 
+
+
+
+from types import new_class
+
+
+list_nums = [0,1,2,3,4,5]
+
+list_2d = [
+    [1,2,3,5,9],
+    [5,"9","45",80,75],
+    [20,10,"12",3,4],
+            ]
+
+# for row in list_2d:
+#     for number in row:
+#         print(number,end=" - ")
+#     print("\n-----------")
+
+
+# for i in range(len(list_2d)):
+    
+#     for j in range(len(list_2d[i])):
+        
+#         print(list_2d[i][j] , end=" - ")
+#     print("\n------------")
+
+# for chizi in list_nums:
+#     print(chizi)
+        
+
+# for i in [0,1,2,3,4,5]:
+#     print(list_nums[i])
+
+# >>> type(range(6))
+# <class 'range'>
+# >>> type(list_nums)
+# <class 'list'>
+    
+
+    
+
+
+# for
+# len()
+# range(from , to ,step)
+
+
+
+for number in list_nums:
+    print(number)
+
+
+
+
+
+
+# Numbers :
+# binary 1010111
+# decimal 250
+# hex base 16 0x4a8
+
+# >>> normal_num = 350
+# >>> normal_num
+# 350
+# >>> bin(normal_num)
+# '0b101011110'
+# >>> hex(normal_num)
+# '0x15e'
+# >>> int('0x15e'
+# ... 
+# KeyboardInterrupt
+# >>> int('15e' , 16)
+# 350
+# >>> int('15e' , 16)
+
+
+
+
+
+
+# type casting
+
+# if 
+# condition 
+# > < = 
+# logic 
+# not or and
+
+# data structure
+# list
+# [213 , "asd" ,1.5]
+# for varaible in list :
+#   print(varaible)
+# range(form , to , step)
+# 
+
+input()
+
+
+
+
+
+
+
+
+
